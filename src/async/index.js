b6921ff10190020ee3642a755df446508497a915
gustavojuan <gustavojuan@gmail.com>
2023-03-13 14:29:32 +0100

    Async Await teoria y ejemplos

---
+++ src/async/index.js
@@ -0,0 +1,20 @@
+const fnAsync = () => {
+    return new Promise((resolve, reject)=> {
+        (true) 
+            ? setTimeout(()=>resolve ('Async!'),2000)
+            : reject ('woops');
+    })   
+}
+
+
+
+//Async y await no bloquean
+const anotherFn = async () =>{
+    const  something = await fnAsync();
+    console.log(something);
+    console.log('hello!');
+}
+
+console.log('Before');
+anotherFn();
+console.log('After');
